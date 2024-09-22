72db80ef898174b5a1e78fb74dacce1105965b1b
bruch-alex <alex.bruch@tutanota.com>
2024-09-22 14:21:41 +0200

    первый коммит

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# awg-ips-converter
+# IP адреса для AWG
+
+> Список ip адресов взят с [RockBlack](https://rockblack.su/vpn/dopolnitelno/diapazon-ip-adresov)
+
+## Списки ip конвертированные для AmneziaWG
+
+[Youtube](src/resources/awg_ips/youtube_and_tv.json)
+
+[ChatGPT](src/resources/awg_ips/chatGPT.json)
+
+[Discord](src/resources/awg_ips/discord.json)
+
+[Instagram](src/resources/awg_ips/instagram.json)
+
+[Twitter](src/resources/awg_ips/twitter.json)
+
+[Netflix](src/resources/awg_ips/netflix.json)
---
+++ src/main/Converter.java
@@ -0,0 +1,41 @@
+package main;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Converter {
+    private static final Pattern ip = Pattern.compile("[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}(/[0-9]{1,3})?");
+
+    public static void main(String[] args) throws IOException {
+        File folder = new File("./src/resources/ranges/");
+        File[] listOfFiles = folder.listFiles();
+
+        for (var file : listOfFiles) {
+            convertFile(file);
+        }
+        System.out.println("Done");
+    }
+
+    private static void convertFile(File file) throws IOException {
+        String input = Files.readString(file.toPath());
+        StringBuilder result = new StringBuilder();
+
+        Matcher matcher = ip.matcher(input);
+
+        result.append("[\n");
+        while (matcher.find()) {
+            result.append("{\"hostname\": \"").append(matcher.group()).append("\",\"ip\":\"\"},\n");
+        }
+        result.deleteCharAt(result.length() - 2).append(']'); // delete last coma and add ]
+
+        try (FileWriter output = new FileWriter("./src/resources/awg_ips/" + file.getName().replace(".txt", ".json"))) {
+            output.write(result.toString());
+        }
+        System.out.println("File saved");
+    }
+
+}
